a1749539f8eb587ab806c49eb36f1a3046ab1e73
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 14:06:47 +0200

    Create baserepository

---
+++ Portfolio.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Portfolio.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Infrastructure.Repositories
+{
+    public class BaseRepository : IBaseRepository
+    {
+    }
+}
