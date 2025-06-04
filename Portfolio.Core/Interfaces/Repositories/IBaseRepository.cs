c8c4b1ec0828125039e7758fb91d696e346aeede
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 13:48:17 +0200

    Create ibaserepository

---
+++ Portfolio.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<bool> AddAsync(T toAdd);
+        Task<bool> UpdateAsync(T toUpdate);
+        Task<bool> DeleteAsync(T toDelete);
+        Task<bool> CheckIfExistsAsync(int id);
+    }
+}
