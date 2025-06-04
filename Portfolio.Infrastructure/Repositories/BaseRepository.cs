2c4ae41b58b244440dcc6fa4a8f700d5fcf80904
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 14:11:34 +0200

    Impliment ibaserepository on baserepository

--- Portfolio.Infrastructure/Repositories/BaseRepository.cs
+++ Portfolio.Infrastructure/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Core.Interfaces.Repositories;
+﻿using Portfolio.Core.Entities;
+using Portfolio.Core.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Portfolio.Infrastructure.Repositories
 {
-    public class BaseRepository : IBaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        public Task<bool> AddAsync(T toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckIfExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(T toDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T toUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
