1c5c9875403aea2bcc7612086511251f9018a42c
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 13:37:03 +0200

    Create base education project and user entities

---
+++ Portfolio.Core/Entities/Education.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Entities
+{
+    public class Education: BaseEntity
+    {
+        public string School {  get; set; }
+        public string Training { get; set; }
+        public string Degree { get; set; }
+    }
+}
