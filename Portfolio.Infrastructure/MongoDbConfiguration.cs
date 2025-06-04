d9dc8675f8b1653a80d8bbb6b067c47e25aacc7f
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 14:46:35 +0200

    Add mongodb to services

---
+++ Portfolio.Infrastructure/MongoDbConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Core.Interfaces.Repositories;
+using Portfolio.Infrastructure.Data;
+using Portfolio.Infrastructure.Repositories;
+using Portfolio.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Infrastructure
+{
+    public static class MongoDbConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<MongoDb>(config.GetSection("MongoDb"));
+            services.AddSingleton<MongoDbContext>();
+
+            return services;
+        }
+    }
+}
