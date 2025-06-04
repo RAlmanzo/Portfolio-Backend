6a2147ea37900437c77e0457f7967e5a7c23dbf6
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-04 14:54:56 +0200

    Inject mongodbcontext in baserepository

--- Portfolio.Infrastructure/Repositories/BaseRepository.cs
+++ Portfolio.Infrastructure/Repositories/BaseRepository.cs
@@ -1,5 +1,7 @@
-﻿using Portfolio.Core.Entities;
+﻿using MongoDB.Driver;
+using Portfolio.Core.Entities;
 using Portfolio.Core.Interfaces.Repositories;
+using Portfolio.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,14 @@ namespace Portfolio.Infrastructure.Repositories
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        private readonly IMongoCollection<T> _collection;
+
+        public BaseRepository(MongoDbContext context)
+        {
+            string collectionTarget = typeof(T).Name + "s";
+            _collection = context.GetCollection<T>(collectionTarget);
+        }
+
         public Task<bool> AddAsync(T toAdd)
         {
             throw new NotImplementedException();
