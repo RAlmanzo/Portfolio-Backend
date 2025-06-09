69679d5e85e04dc7fc8623fad8bb384b248fe807
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-09 19:09:41 +0200

    Create dto emailrequestdto

---
+++ Portfolio.Api/Dtos/EmailRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Api.Dtos
+{
+    public class EmailRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string From { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [MaxLength(5000)]
+        public string Message { get; set; }
+    }
+}
