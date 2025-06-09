ab5fdd3237091991e9e7f68948c2f757c2f33687
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-09 14:52:52 +0200

    Create new interface iprojectservice

---
+++ Portfolio.Core/Interfaces/Services/IProjectService.cs
@@ -0,0 +1,14 @@
+﻿using Portfolio.Core.Entities;
+using Portfolio.Core.Services.Models;
+
+namespace Portfolio.Core.Interfaces.Services
+{
+    public interface IProjectService
+    {
+        Task<ResultModel<Project>> GetByIdAsync(string id);
+        Task<ResultModel<IEnumerable<Project>>> GetAllAsync();
+        Task<ResultModel<Project>> CreateProjectAsync(ProjectCreateRequestModel ProjectCreateRequestModel);
+        Task<ResultModel<Project>> UpdateProjectAsync(ProjectUpdateRequestModel ProjectUpdateRequestModel);
+        Task<ResultModel<Project>> DeleteProjectAsync(string id);
+    }
+}
