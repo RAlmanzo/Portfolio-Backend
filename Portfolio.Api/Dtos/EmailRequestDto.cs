cd7d3635bb6988983d0cca03dfb39abbf7d43469
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-12 18:24:43 +0200

    Add property name and change name of from to email

--- Portfolio.Api/Dtos/EmailRequestDto.cs
+++ Portfolio.Api/Dtos/EmailRequestDto.cs
@@ -5,8 +5,12 @@ namespace Portfolio.Api.Dtos
     public class EmailRequestDto
     {
         [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
         [EmailAddress]
-        public string From { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [MaxLength(100)]
