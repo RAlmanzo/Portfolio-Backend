9ed8afdb56882e5b820c11295f4cd1e546f80d1f
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-09 20:25:02 +0200

    Create new class userservice and implement iuserservice interface

---
+++ Portfolio.Infrastructure/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Portfolio.Core.Entities;
+using Portfolio.Core.Interfaces.Services;
+using Portfolio.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<ResultModel<User>> CreateUserAsync(UserCreateRequestModel UserCreateRequestModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<User>> DeleteUserAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<IEnumerable<User>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<User>> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<User>> UpdateUserAsync(UserUpdateRequestModel UserUpdateRequestModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
