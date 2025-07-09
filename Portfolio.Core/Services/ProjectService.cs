bcd5372f0333de8cb70d8e25261e4e3728375208
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-09 20:55:49 +0200

    Edit getbyidasync implementation

--- Portfolio.Core/Services/ProjectService.cs
+++ Portfolio.Core/Services/ProjectService.cs
@@ -1,4 +1,5 @@
 ﻿using Portfolio.Core.Entities;
+using Portfolio.Core.Interfaces.Repositories;
 using Portfolio.Core.Interfaces.Services;
 using Portfolio.Core.Services.Models;
 using System;
@@ -11,6 +12,35 @@ namespace Portfolio.Core.Services
 {
     public class ProjectService : IProjectService
     {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectService(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<ResultModel<Project>> GetByIdAsync(string id)
+        {
+            //get the project
+            var project = await _projectRepository.GetByIdAsync(id);
+
+            //create new resultmodel
+            var projectResultModel = new ResultModel<Project>();
+
+            //check if exists
+            if (project == null)
+            {
+                projectResultModel.Success = false;
+                projectResultModel.Errors = ["No project found!"];
+                return projectResultModel;
+            }
+
+            //if exists
+            projectResultModel.Success = true;
+            projectResultModel.Value = project;
+            return projectResultModel;
+        }
+
         public Task<ResultModel<Project>> CreateProjectAsync(ProjectCreateRequestModel ProjectCreateRequestModel)
         {
             throw new NotImplementedException();
@@ -22,11 +52,6 @@ namespace Portfolio.Core.Services
         }
 
         public Task<ResultModel<IEnumerable<Project>>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResultModel<Project>> GetByIdAsync(string id)
         {
             throw new NotImplementedException();
         }
