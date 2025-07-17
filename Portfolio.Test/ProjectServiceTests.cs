a194bb13376175b1c115f7ef6ab54631c1c7acfe
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-17 14:45:55 +0200

    Mock iprojectrepository and create new instance of projectservice

---
+++ Portfolio.Test/ProjectServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using Portfolio.Core.Interfaces.Repositories;
+using Portfolio.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Portfolio.Tests
+{
+    public class ProjectServiceTests
+    {
+        private readonly Mock<IProjectRepository> _mockProjectRepository;
+        private readonly ProjectService _projectService;
+
+        public ProjectServiceTests()
+        {
+            _mockProjectRepository = new Mock<IProjectRepository>();
+            _projectService = new ProjectService(_mockProjectRepository.Object);
+        }
+
+
+
+        [Fact]
+        public async Task GetByIdAsync_WithExistingProject_ReturnsProject()
+        {
+
+        }
+    }
+}
