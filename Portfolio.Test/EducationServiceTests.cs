8baa6a220c86bd72bf52e9906cc346b8e507ff8a
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-01 23:54:53 +0200

    Create unit test for case getbyidasync withexistingeducation returnseducation

--- Portfolio.Test/EducationServiceTests.cs
+++ Portfolio.Test/EducationServiceTests.cs
@@ -1,13 +1,17 @@
 ﻿using Castle.Core.Logging;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Portfolio.Core.Entities;
 using Portfolio.Core.Interfaces.Repositories;
+using Portfolio.Core.Interfaces.Services;
 using Portfolio.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Portfolio.Tests
 {
@@ -22,6 +26,25 @@ namespace Portfolio.Tests
             _mockEducationRepository = new Mock<IEducationRepository>();
             _mockLogger = new Mock<ILogger<EducationService>>();
             _educationService = new EducationService(_mockEducationRepository.Object, _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WithExistingEducation_ReturnsEducation()
+        {
+            //Arrange
+            var educationId = "abc123";
+            var selectedEducation = new Education { Id = educationId, Degree = "test data", School = "test school" };
+
+            _mockEducationRepository.Setup(repo => repo.GetByIdAsync(educationId)).ReturnsAsync(selectedEducation);
+
+            //Act
+
+            var result = await _educationService.GetByIdAsync(educationId);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+            result.Errors.Should().BeNullOrEmpty();
         }
     }
 }
