b98875a7e93b26b0a98b0b75c4094acfd954c7b8
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-12 18:24:43 +0200

    Add resumeid property and delete educations property

--- Portfolio.Core/Entities/User.cs
+++ Portfolio.Core/Entities/User.cs
@@ -8,20 +8,20 @@ namespace Portfolio.Core.Entities
 {
     public class User : BaseEntity
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public DateTime DateOfBirth { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Description { get; set; }
-        public string Nationality { get; set; }
-        public string DriverLicense { get; set; }
-        public string Residence { get; set; }
-        public string GitHub {  get; set; }
-        public string Linkedin { get; set; }
-        public ICollection<string> Images { get; set; }
-        public ICollection<Project> Projects { get; set; }
-        public ICollection<string> Languages { get; set; }
-        public ICollection<Education> Educations { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Nationality { get; set; } = string.Empty;
+        public string DriverLicense { get; set; } = string.Empty;
+        public string Residence { get; set; } = string.Empty;
+        public string GitHub { get; set; } = string.Empty;
+        public string Linkedin { get; set; } = string.Empty;
+        public string ResumeId { get; set; } = string.Empty;
+        public ICollection<string> Images { get; set; } = [];
+        public ICollection<Project> Projects { get; set; } = [];
+        public ICollection<string> Languages { get; set; } = [];
     }
 }
