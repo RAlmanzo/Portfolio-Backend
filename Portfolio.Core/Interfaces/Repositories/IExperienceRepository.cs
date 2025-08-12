3f8c1da292f4734962f0a9a6946e8494408c9454
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-12 18:51:42 +0200

    Let iexperiencerepository inherit from ibaserepository

--- Portfolio.Core/Interfaces/Repositories/IExperienceRepository.cs
+++ Portfolio.Core/Interfaces/Repositories/IExperienceRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Portfolio.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Portfolio.Core.Interfaces.Repositories
 {
-    public interface IExperienceRepository
+    public interface IExperienceRepository : IBaseRepository<Experience>
     {
     }
 }
