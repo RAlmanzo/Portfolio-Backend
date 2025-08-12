f01210d0ad526bad58bbdca28be1e2f005a88fe2
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-12 18:23:39 +0200

    Add properties to resume class

--- Portfolio.Core/Entities/Resume.cs
+++ Portfolio.Core/Entities/Resume.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Portfolio.Core.Entities
 {
-    public class Resume
+    public class Resume : BaseEntity
     {
+        public required string UserId { get; set; }
+        public ICollection<Skill> Skills { get; set; } = [];
+        public ICollection<Education> Educations { get; set; } = [];
+        public ICollection<Experience> Experiences { get; set; } = [];
     }
 }
