239e407aafb14ea577753358fd07fc4d07ce4ba3
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-13 19:58:10 +0200

    Mock experiencerepitory experiencesservice and ilogger

--- Portfolio.Test/ExperienceServiceTests.cs
+++ Portfolio.Test/ExperienceServiceTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Portfolio.Core.Interfaces.Repositories;
+using Portfolio.Core.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,15 @@ namespace Portfolio.Tests
 {
     public class ExperienceServiceTests
     {
+        private readonly Mock<IExperienceRepository> _mockExperienceRepository;
+        private readonly Mock<ILogger<ExperienceService>> _mockLogger;
+        private readonly ExperienceService _experienceService;
+        public ExperienceServiceTests()
+        {
+            _mockExperienceRepository = new Mock<IExperienceRepository>();
+            _mockLogger = new Mock<ILogger<ExperienceService>>();
+            _experienceService = new ExperienceService(_mockExperienceRepository.Object, _mockLogger.Object);
+        }
+
     }
 }
