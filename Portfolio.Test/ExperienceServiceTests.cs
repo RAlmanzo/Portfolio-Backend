0a9e49d54aab7457b7dd571f5f407b97e465843c
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-13 20:03:17 +0200

    Create for experienceservice new test case getbyidasync withexistingeducation returnseducation

--- Portfolio.Test/ExperienceServiceTests.cs
+++ Portfolio.Test/ExperienceServiceTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Moq;
+using Portfolio.Core.Entities;
 using Portfolio.Core.Interfaces.Repositories;
 using Portfolio.Core.Services;
 using System;
@@ -7,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Portfolio.Tests
 {
@@ -22,5 +25,29 @@ namespace Portfolio.Tests
             _experienceService = new ExperienceService(_mockExperienceRepository.Object, _mockLogger.Object);
         }
 
+        [Fact]
+        public async Task GetByIdAsync_WithExistingExperience_ReturnsExperience()
+        {
+            // Arrange
+            var experienceId = "abc123";
+            var selectedExperience = new Experience 
+            { 
+                Id = experienceId, 
+                Position = "Software Engineer", 
+                Company = "Tech Corp", 
+                Location = "New York, NY",
+                StartDate = new DateTime(2020, 1, 1),
+            };
+
+            _mockExperienceRepository.Setup(repo => repo.GetByIdAsync(experienceId)).ReturnsAsync(selectedExperience);
+
+            // Act
+            var result = await _experienceService.GetByIdAsync(experienceId);
+
+            // Assert
+            result.Success.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+            result.Errors.Should().BeNullOrEmpty();
+        }
     }
 }
