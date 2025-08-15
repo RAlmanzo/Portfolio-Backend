4003efedc3d52489a400cbd45e054f8ab9083b5a
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-15 19:29:11 +0200

    Add properties for experiencerequestmodel

--- Portfolio.Core/Services/Models/ExperienceCreateRequestModel.cs
+++ Portfolio.Core/Services/Models/ExperienceCreateRequestModel.cs
@@ -8,5 +8,11 @@ namespace Portfolio.Core.Services.Models
 {
     public class ExperienceCreateRequestModel
     {
+        public required string Location { get; set; }
+        public required string Company { get; set; }
+        public required DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public required string Position { get; set; }
+        public string? Information { get; set; }
     }
 }
